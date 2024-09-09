9047138085a000c2fceb88a6c00f0bdcb778c3bf
Dave <davejoshuamiguel2@gmail.com>
2024-09-09 04:34:15 -0400

    Final Changes

--- Rock Paper Scissor/java.js
+++ Rock Paper Scissor/java.js
@@ -1,4 +1,3 @@
-
 const choices = [
     { name: 'rock', image: '/Rock Paper Scissor/img/rock.webp' },
     { name: 'paper', image: '/Rock Paper Scissor/img/paper.webp' },
@@ -11,14 +10,11 @@ const decision = [
 ];
 
 
-
-
 const p1Score = document.getElementById('1P-score');
 const p2Score = document.getElementById('2P-score');
 const p1Option = document.getElementById('player1-img');
-const p2Option = document.getElementById('player2-img');
-const resulOutcome = document.getElementById('outcome');
-
+const p2OPtion = document.getElementById('player2-img');
+const outcome = document.getElementById('outcome');
 
 let player1Score = 0;
 let player2Score = 0;
@@ -28,10 +24,10 @@ function getRandomChoice() {
     return choices[Math.floor(Math.random() * choices.length)];
 }
 
+
 function endResult(player1Choice, player2Choice) {
     if (player1Choice.name === player2Choice.name) {
-       
-        resulOutcome.innerHTML = `<img src="${decision[0].image}" alt="Draw" style="width: 80%;"> <h3>DRAW!!</h3>`;
+        outcome.innerHTML = `<img src="${decision[0].image}" alt="Draw" style="width: 80%;"> <h3>DRAW!!</h3>`;
         return 'DRAW!!';
     } else if (
         (player1Choice.name === 'rock' && player2Choice.name === 'scissors') ||
@@ -39,27 +35,26 @@ function endResult(player1Choice, player2Choice) {
         (player1Choice.name === 'scissors' && player2Choice.name === 'paper')
     ) {
         player1Score++;
-        resulOutcome.innerHTML = `<img src="${decision[1].image}" alt="Player 1 Wins" style="width: 80%;"> <h3>PLAYER 1 WINS!!</h3>`;
+        outcome.innerHTML = `<img src="${decision[1].image}" alt="Player 1 Wins" style="width: 80%;"> <h3>PLAYER 1 WINS!!</h3>`;
         return 'PLAYER 1 WINS!!';
     } else {
         player2Score++;
-        resulOutcome.innerHTML = `<img src="${decision[1].image}" alt="Player 2 Wins" style="width: 80%;"> <h3>PLAYER 2 WINS!!</h3>`;
+        outcome.innerHTML = `<img src="${decision[1].image}" alt="Player 2 Wins" style="width: 80%;"> <h3>PLAYER 2 WINS!!</h3>`;
         return 'PLAYER 2 WINS!!';
     }
 }
-
-
 
 
 function runGame() {
     const player1Choice = getRandomChoice();
     const player2Choice = getRandomChoice();
    
+   
     p1Option.src = player1Choice.image;
-    p2Option.src = player2Choice.image;
-    
-    endResult   (player1Choice, player2Choice);
+    p2OPtion.src = player2Choice.image;
 
+    endResult(player1Choice, player2Choice);
+  
     p1Score.innerHTML = `<h1>${player1Score}</h1>`;
     p2Score.innerHTML = `<h1>${player2Score}</h1>`;
 }
